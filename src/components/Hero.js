81b93acfafa9c5413e20f1dd0cd831ee103c9e46
Joseph Semaan Sfeir <202311768@ua.edu.lb>
2024-07-23 22:30:41 +0300

    Header visual upgrades

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,6 +9,7 @@ const Hero = () => {
             <div className="circle-2"></div>
             <div className="circle-3"></div>
             <div className="circle-4"></div>
+            <div className="circle-5"></div>
             <div className="circle-top-right "></div>
             <div className="hero-image">
                 <img src={`${process.env.PUBLIC_URL}/sexy.jpeg`} alt="Sexy" />
