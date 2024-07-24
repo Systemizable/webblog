c38c3bc5b21cdbb544179093fc17baeeef89429a
Joseph Semaan Sfeir <202311768@ua.edu.lb>
2024-07-25 00:45:08 +0300

    Changes on mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,11 @@ const Header = () => (
     <header className="header">
         <div className="header-content">
             <h1>Joseph Sfeir</h1>
+            <div className="subheadings">
+                <h2 className="subheading subheading-constant-learner">Constant Learner</h2>
+                <h3 className="subheading subheading-avid-programmer">Avid Programmer</h3>
+            </div>
         </div>
-        <div className="circle-1"></div>
-        <div className="circle-2"></div>
-        <div className="circle-3"></div>
-        <div className="circle-4"></div>
-        <div className="circle-5"></div>
-        <div className="circle-6"></div>
         <nav className="nav-links">
             <ul>
                 <li><a href="#about">About Me</a></li>
