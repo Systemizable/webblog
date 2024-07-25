fe2f64a0aaa7bfcb7ff76c04aedcb415346109fd
Joseph Semaan Sfeir <202311768@ua.edu.lb>
2024-07-25 13:39:35 +0300

    Added timeline to education

--- src/components/Education.js
+++ src/components/Education.js
@@ -3,9 +3,55 @@ import './Education.css';
 
 const Education = () => (
     <section className="education" id="education">
+        <div className="timeline-container">
+            <div className="timeline">
+                <div className="timeline-event">
+                    <div className="timeline-content">
+                        <h3>Attended Antonine School</h3>
+                        <p>2008</p>
+                    </div>
+                </div>
+                <div className="timeline-event">
+                    <div className="timeline-content">
+                        <h3>Introduced to Modding</h3>
+                        <p>2011</p>
+                    </div>
+                </div>
+                <div className="timeline-event">
+                    <div className="timeline-content">
+                        <h3>Discovered Game Development</h3>
+                        <p>2015</p>
+                    </div>
+                </div>
+                <div className="timeline-event">
+                    <div className="timeline-content">
+                        <h3>Decided That's the Career to Pursue</h3>
+                        <p>2018</p>
+                    </div>
+                </div>
+                <div className="timeline-event">
+                    <div className="timeline-content">
+                        <h3>Started Learning Programming</h3>
+                        <p>2020</p>
+                    </div>
+                </div>
+                <div className="timeline-event">
+                    <div className="timeline-content">
+                        <h3>Graduated</h3>
+                        <p>2023</p>
+                    </div>
+                </div>
+                <div className="timeline-event">
+                    <div className="timeline-content">
+                        <h3>Attended UA</h3>
+                        <p>2023</p>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div className="education-text">
             <h2>Education</h2>
-            <p>Growing up in Zahle, Lebanon, it just felt normal for me that i should be enrolled in Antonine Sisters School due to it being one of the most prominent schools in the city and possibly in the country. My early academic journey was filled with straight A's, and constant exemptions from taking final exams, and this carried through till the 9th grade. When COVID hit, I just felt drained and no longer felt the need to study as we're now online-learning. This prompted me to start discovering Programming, mostly due to my brother's major which is also Computer and Communications Engineering.  </p>
+            <p>Growing up in Zahle, Lebanon, it just felt normal for me that I should be enrolled in Antonine Sisters School due to it being one of the most prominent schools in the city and possibly in the country. My early academic journey was filled with straight A's, and constant exemptions from taking final exams, and this carried through till the 9th grade. When COVID hit, I just felt drained and no longer felt the need to study as we're now online-learning. This prompted me to start discovering Programming, mostly due to my brother's major which is also Computer and Communications Engineering.</p>
         </div>
     </section>
 );
