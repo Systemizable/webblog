eb225edee6c0cc2dcfdcb1c9c48733b7e2bd2583
Joseph Semaan Sfeir <202311768@ua.edu.lb>
2024-07-26 13:37:54 +0300

    Photo change

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,8 +5,21 @@ const Skills = () => (
     <section className="skills" id="skills">
         <h2>Skills</h2>
         <div className="skills-content">
-            <p>Hey, my name is Joseph Sfeir, I'm a 19 year old programmer, who's currently majoring in Computer and Communications EngineeringHey, my name is Joseph Sfeir, I'm a 19 year old programmer, who's currently majoring in Computer and Communications EngineeringHey, my name is Joseph Sfeir, I'm a 19 year old programmer, who's currently majoring in Computer and Communications EngineeringHey, my name is Joseph Sfeir, I'm a 19 year old programmer, who's currently majoring in Computer and Communications EngineeringHey, my name is Joseph Sfeir, I'm a 19 year old programmer, who's currently majoring in Computer and Communications EngineeringHey, my name is Joseph Sfeir, I'm a 19 year old programmer, who's currently majoring in Computer and Communications EngineeringHey, my name is Joseph Sfeir, I'm a 19 year old programmer, who's currently majoring in Computer and Communications EngineeringHey, my name is Joseph Sfeir, I'm a 19 year old programmer, who's currently majoring in Computer and Communications EngineeringHey, my name is Joseph Sfeir, I'm a 19 year old programmer, who's currently majoring in Computer and Communications EngineeringHey, my name is Joseph Sfeir, I'm a 19 year old programmer, who's currently majoring in Computer and Communications EngineeringHey, my name is Joseph Sfeir, I'm a 19 year old programmer, who's currently majoring in Computer and Communications EngineeringHey, my name is Joseph Sfeir, I'm a 19 year old programmer, who's currently majoring in Computer and Communications EngineeringHey, my name is Joseph Sfeir, I'm a 19 year old programmer, who's currently majoring in Computer and Communications EngineeringHey, my name is Joseph Sfeir, I'm a 19 year old programmer, who's currently majoring in Computer and Communications EngineeringHey, my name is Joseph Sfeir, I'm a 19 year old programmer, who's currently majoring in Computer and Communications EngineeringHey, my name is Joseph Sfeir, I'm a 19 year old programmer, who's currently majoring in Computer and Communications EngineeringHey, my name is Joseph Sfeir, I'm a 19 year old programmer, who's currently majoring in Computer and Communications EngineeringHey, my name is Joseph Sfeir, I'm a 19 year old programmer, who's currently majoring in Computer and Communications EngineeringHey, my name is Joseph Sfeir, I'm a 19 year old programmer, who's currently majoring in Computer and Communications EngineeringHey, my name is Joseph Sfeir, I'm a 19 year old programmer, who's currently majoring in Computer and Communications EngineeringHey, my name is Joseph Sfeir, I'm a 19 year old programmer, who's currently majoring in Computer and Communications EngineeringHey, my name is Joseph Sfeir, I'm a 19 year old programmer, who's currently majoring in Computer and Communications EngineeringHey, my name is Joseph Sfeir, I'm a 19 year old programmer, who's currently majoring in Computer and Communications EngineeringHey, my name is Joseph Sfeir, I'm a 19 year old programmer, who's currently majoring in Computer and Communications Engineering</p>
-
+            <div className="web">
+                <div className="web-circle web-circle-1"></div>
+                <div className="web-circle web-circle-2"></div>
+                <div className="web-circle web-circle-3"></div>
+                <div className="web-circle web-circle-4"></div>
+                <a href="#css" className="skill" style={{ top: '10%', left: '50%' }}>CSS</a>
+                <a href="#advanced-cpp" className="skill" style={{ top: '20%', left: '70%' }}>Advanced C++</a>
+                <a href="#problem-solving" className="skill" style={{ top: '40%', left: '90%' }}>Problem Solving</a>
+                <a href="#debating" className="skill" style={{ top: '60%', left: '70%' }}>Debating</a>
+                <a href="#java" className="skill" style={{ top: '80%', left: '50%' }}>Java</a>
+                <a href="#javascript" className="skill" style={{ top: '60%', left: '30%' }}>JavaScript</a>
+                <a href="#python" className="skill" style={{ top: '40%', left: '10%' }}>Python</a>
+                <a href="#react" className="skill" style={{ top: '20%', left: '30%' }}>React</a>
+                <a href="#html" className="skill" style={{ top: '50%', left: '50%' }}>HTML</a>
+            </div>
         </div>
     </section>
 );
