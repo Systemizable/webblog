b77f390e7ccc43412bf129fbf98e70c91e34673b
Systemizable <202311768@ua.edu.lb>
2024-07-27 22:33:03 +0300

    Fixed scroll issues

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ const Header = () => (
             <ul>
                 <li><a href="#about">About Me</a></li>
                 <li><a href="#education">Education</a></li>
-                <li><a href="#skills">Skills & Experience</a></li>
+                <li><a href="#skills">Skills</a></li>
                 <li><a href="#projects">Projects</a></li>
                 <li><a href="#git">GitHub</a></li>
                 <li><a href="#contact">Contact</a></li>
