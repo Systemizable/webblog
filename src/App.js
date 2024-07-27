25fe5a17882cc5e0d13b520de1a1bd3f6c5a42fa
Joseph Semaan Sfeir <202311768@ua.edu.lb>
2024-07-27 19:02:20 +0300

    Few visual changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+// App.js
 import React from 'react';
+import { Analytics } from '@vercel/analytics/react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
@@ -7,29 +9,29 @@ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Blog from './components/Blog';
 import Footer from './components/Footer';
-
 import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <Header />
-            <hr className="separator" />
-            <Hero />
-            <hr className="separator" />
-            <About />
-            <hr className="separator" />
-            <Education />
-            <hr className="separator" />
-            <Skills />
-            <hr className="separator" />
-            <Projects />
-            <hr className="separator" />
-            <Blog />
-            <hr className="separator" />
-            <Footer />
-        </div>
-    );
+        return (
+            <div className="App">
+                    <Header />
+                    <hr className="separator" />
+                    <Hero />
+                    <hr className="separator" />
+                    <About />
+                    <hr className="separator" />
+                    <Education />
+                    <hr className="separator" />
+                    <Skills />
+                    <hr className="separator" />
+                    <Projects />
+                    <hr className="separator" />
+                    <Blog />
+                    <hr className="separator" />
+                    <Footer />
+                    <Analytics />
+            </div>
+        );
 }
 
 export default App;
