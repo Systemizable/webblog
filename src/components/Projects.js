f7058f22d11932fa899d3f9d4b84db543150d547
Systemizable <202311768@ua.edu.lb>
2024-08-22 17:47:30 +0300

    Added project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -29,35 +29,55 @@ const Projects = () => (
                 }
             >
                 <div className="slide-container">
-                    <img src={`${process.env.PUBLIC_URL}/math.png`} alt="Math Menu Operations" className="slide-image" />
+                    <img src={`${process.env.PUBLIC_URL}/ecom.png`} alt="E-Commerce Website" className="slide-image"/>
+                    <div className="difficulty-circle advanced" title="Advanced"></div>
+                    <div className="text-container">
+                        <h3>E-Commerce Website</h3>
+                        <p>One of my first very advanced projects I've done. This E-Commerce website contains a detailed
+                            home page, with a shopping page that contains all products, with the ability to check
+                            individual products.</p>
+                        <a href="https://jarvis-ecom.netlify.app" target="_blank" rel="noopener noreferrer">Check out
+                            Jarvis E-Com!</a>
+                    </div>
+                </div>
+
+                <div className="slide-container">
+                    <img src={`${process.env.PUBLIC_URL}/math.png`} alt="Math Menu Operations" className="slide-image"/>
                     <div className="difficulty-circle beginner" title="Beginner"></div>
                     <div className="text-container">
                         <h3>Math Menu Operations</h3>
-                        <p>This is a simple project that represents my first ever coding project. Even though it's extremely basic, it holds a special place as the foundation of my programming journey.</p>
+                        <p>This is a simple project that represents my first ever coding project. Even though it's
+                            extremely basic, it holds a special place as the foundation of my programming journey.</p>
                     </div>
                 </div>
                 <div className="slide-container">
-                    <img src={`${process.env.PUBLIC_URL}/arduino.png`} alt="Water Level Sensor" className="slide-image" />
+                    <img src={`${process.env.PUBLIC_URL}/arduino.png`} alt="Water Level Sensor"
+                         className="slide-image"/>
                     <div className="difficulty-circle intermediate" title="Intermediate"></div>
                     <div className="text-container">
                         <h3>Water Level Sensor</h3>
-                        <p>This is an introduction to engineering project. We were tasked, in any shape or form, to create a water level sensor that gave out signals when it reached certain levels. We opted for an Arduino, LCD, and sonic sensor, and found major success!</p>
+                        <p>This is an introduction to engineering project. We were tasked, in any shape or form, to
+                            create a water level sensor that gave out signals when it reached certain levels. We opted
+                            for an Arduino, LCD, and sonic sensor, and found major success!</p>
                     </div>
                 </div>
                 <div className="slide-container">
-                    <img src={`${process.env.PUBLIC_URL}/car.png`} alt="Car Renting Program" className="slide-image" />
-                    <div className="difficulty-circle advanced" title="Advanced"></div>
+                    <img src={`${process.env.PUBLIC_URL}/car.png`} alt="Car Renting Program" className="slide-image"/>
+                    <div className="difficulty-circle intermediate" title="Advanced"></div>
                     <div className="text-container">
                         <h3>Car Renting Program</h3>
-                        <p>This is a Programming 2 project. We were tasked to create in C++, a car rental program that would save user credentials, provide encryption for passwords, save rental details, and alter a PDF file containing rental info.</p>
+                        <p>This is a Programming 2 project. We were tasked to create in C++, a car rental program that
+                            would save user credentials, provide encryption for passwords, save rental details, and
+                            alter a PDF file containing rental info.</p>
                     </div>
                 </div>
                 <div className="slide-container">
-                    <img src={`${process.env.PUBLIC_URL}/port.png`} alt="Portfolio Website" className="slide-image" />
-                    <div className="difficulty-circle intermediate" title="Intermediate"></div>
+                    <img src={`${process.env.PUBLIC_URL}/port.png`} alt="Portfolio Website" className="slide-image"/>
+                    <div className="difficulty-circle advanced" title="Intermediate"></div>
                     <div className="text-container">
                         <h3>Portfolio Website</h3>
-                        <p>This website will be added to my projects in the future, showcasing my skills and portfolio as a programmer.</p>
+                        <p>This website will be added to my projects in the future, showcasing my skills and portfolio
+                            as a programmer.</p>
                     </div>
                 </div>
             </Carousel>
